128e50da4b4ff3a39509cb129de427a54ab049ce
deress <idrismuhammad257@gmail.com>
2023-11-17 14:30:01 +0700

    menambahkan beasiswa activity

---
+++ app/src/main/java/com/dicoding/rosterseagames/BeasiswaActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.rosterseagames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BeasiswaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beasiswa)
+    }
+}
